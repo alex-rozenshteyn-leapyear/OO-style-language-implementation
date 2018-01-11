147a68bf8687e3b1ff0a4f1fcdadd7ef53f0e934
Alex Rozenshteyn <rpglover64@gmail.com>
2018-01-10 22:37:14 -0800

    Factor out arithObj

--- Obj.hs
+++ Obj.hs
@@ -9,7 +9,10 @@ mkObj :: Expr -> Obj
 mkObj = \case
   B b -> Obj $ Just $ BTag `Tagged` (TObj b)
   I i -> Obj $ Just $ ITag `Tagged` (TObj i)
-  Plus e1 e2 -> Obj . fmap (ITag `Tagged`) $ do
-    Tagged ITag te1 <- check $ mkObj e1
-    Tagged ITag te2 <- check $ mkObj e2
-    return $ TObj $ eval te1 + eval te2
+  Plus e1 e2 -> arithObj (+) e1 e2
+
+arithObj :: (Int -> Int -> Int) -> Expr -> Expr -> Obj
+arithObj f e1 e2 = Obj . fmap (ITag `Tagged`) $ do
+  Tagged ITag te1 <- check $ mkObj e1
+  Tagged ITag te2 <- check $ mkObj e2
+  return $ TObj $ eval te1 `f` eval te2
